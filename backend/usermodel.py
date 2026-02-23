57db761e64a15b24b4f464c3c8ca1dc8961e0903
stuti <up2199677@myport.ac.uk>
2026-02-23 21:32:57 +0000

    django still requires username so set to None

--- backend/usermodel.py
+++ backend/usermodel.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser): 
+    username = None
     up_number = models.CharField(max_length=20, unique=True)
     role = models.CharField(
         max_length=20,
