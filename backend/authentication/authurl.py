3a9c094c7022f62f0978540cb1b4f506ee206cb8
stuti <up2199677@myport.ac.uk>
2026-02-24 19:42:12 +0000

    running the authentication url

--- backend/authentication/authurl.py
+++ backend/authentication/authurl.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from backend.authentication.loginAPIview import LoginView
+from .loginAPIview import LoginView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
