710d823e657a6215101e6483eddda7c302ab686c
stuti <up2199677@myport.ac.uk>
2026-02-24 17:56:41 +0000

    altered loginAPI file

--- backend/authentication/loginAPIview.py
+++ backend/authentication/loginAPIview.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from .services import authenticate_user
 
 class LoginView(APIView):
 
@@ -10,11 +11,7 @@ class LoginView(APIView):
         up_number = request.data.get("up_number")
         password = request.data.get("password")
 
-        user = authenticate(
-            request,
-            username=up_number,
-            password=password
-        )
+        user = authenticate_user(up_number, password)
 
         if user is not None:
             token, created = Token.objects.get_or_create(user=user)
