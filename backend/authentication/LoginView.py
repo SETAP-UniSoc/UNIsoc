8c6655fe902ad621a7bf197f4121502edbbb6a52
stuti <up2199677@myport.ac.uk>
2026-02-26 17:36:42 +0000

    removed unneccessary improts from django framework in auth folder in LoginView.py

--- backend/authentication/LoginView.py
+++ backend/authentication/LoginView.py
@@ -1,12 +1,7 @@
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate
-from django.views.decorators.csrf import csrf_exempt
 import re
 from rest_framework.authtoken.models import Token
 
--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,8 +3,8 @@ from .RegisterView import RegisterView
 from .LoginView import LoginView
 
 urlpatterns = [
-    path("admin/login/", LoginView.as_view(), name="admin-login"),
-    path("user/login/", LoginView.as_view(), name="user-login"),
-    path("user/register/", RegisterView.as_view(), name="user-register"),
+    path("api/admin/login/", LoginView.as_view(), name="admin-login"),
+    path("api/user/login/", LoginView.as_view(), name="user-login"),
+    path("api/user/register/", RegisterView.as_view(), name="user-register"),
 ]
 
