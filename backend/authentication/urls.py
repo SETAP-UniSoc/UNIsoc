952d2fd3852ebb900a11bcc99ffe2861634f25dd
stuti <up2199677@myport.ac.uk>
2026-02-26 14:49:04 +0000

    added registerview to urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .loginAPIview import LoginView
+from .RegisterView import RegisterView, LoginView
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('api/register/', RegisterView.as_view()),
+    path('api/login/', LoginView.as_view()),
 ]
 
