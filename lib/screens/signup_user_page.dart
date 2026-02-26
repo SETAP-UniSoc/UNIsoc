e30fd25193facd26179264ac2d8dcddeee53cd4c
MM674294 <mayamartey@gmail.com>
2026-02-26 16:43:14 +0000

    added backslash to incorrect url i the signup_user file

--- lib/screens/signup_user_page.dart
+++ lib/screens/signup_user_page.dart
@@ -82,7 +82,7 @@ if (!RegExp(r'[@$!%*?&]').hasMatch(password)) {
   _showError("Password must contain at least one special character");
   return;
 }
-    final url = Uri.parse("http://10.128.5.47:8000/api/user/signup");
+    final url = Uri.parse("http://10.128.5.47:8000/api/user/signup/");
 
     try {
       final response = await http.post(
