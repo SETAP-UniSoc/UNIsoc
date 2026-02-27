5d4d7eba57cdc1ee33a52846c712d4acb391bb37
Ty694nite <taiyebahmed694@gmail.com>
2026-02-27 14:58:12 +0000

    Add previously made code to home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+// ...existing code...
+/* import 'package:flutter/material.dart';
+import 'navbar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const HomeNavbar(),
+      body: const Center(
+        child: Text('Home content here'),
+      ),
+    );
+  }
+} */
+// ...existing code...
