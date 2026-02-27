98385f4276a7159b1120cb90d2ac34cf2666773d
stuti <up2199677@myport.ac.uk>
2026-02-27 14:51:33 +0000

    added analytics and join soc url path ot urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .RegisterView import RegisterView
 from .LoginView import LoginView
-from .analytics import JoinSocietyView
+from .AnalyticsView import AnalyticsView, JoinSocietyView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("user/register/", RegisterView.as_view(), name="register"),
     path("society/<int:society_id>/join/", JoinSocietyView.as_view(), name="join-society"),
+    path("analytics/society/<int:society_id>/", AnalyticsView.as_view(), name="society-analytics"),
 
 ]
+
+
 
 
