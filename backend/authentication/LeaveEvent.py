dba9a045569a55f8ac1bec3b9f3c76687bcc1580
stuti <up2199677@myport.ac.uk>
2026-03-02 16:54:28 +0000

    fizxed errors

---
+++ backend/authentication/LeaveEvent.py
@@ -0,0 +1,24 @@
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.permissions import IsAuthenticated
+# from django.utils import timezone
+# from .models import Society, Membership
+
+# class LeaveEventView(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request, event_id):
+#         try:
+#             attendance = EventAttendance.objects.get(
+#                 user=request.user,
+#                 event_id=event_id,
+#                 left_at__isnull=True
+#             )
+#         except EventAttendance.DoesNotExist:
+#             return Response({"error": "Not attending this event"}, status=400)
+
+#         attendance.left_at = timezone.now()
+#         attendance.save()
+
+#         return Response({"message": "Left event successfully"})
