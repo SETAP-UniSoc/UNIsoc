b16c3bc2219da72161946a3773387f6b1d9040bb
stuti <up2199677@myport.ac.uk>
2026-03-02 18:31:52 +0000

    made the leave event.py

--- backend/authentication/LeaveEvent.py
+++ backend/authentication/LeaveEvent.py
@@ -1,24 +1,25 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework.permissions import IsAuthenticated
-# from django.utils import timezone
-# from .models import Society, Membership
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
+from .models import Society, Membership
+from .models import EventAttendance
 
-# class LeaveEventView(APIView):
-#     permission_classes = [IsAuthenticated]
+class LeaveEventView(APIView):
+    permission_classes = [IsAuthenticated]
 
-#     def post(self, request, event_id):
-#         try:
-#             attendance = EventAttendance.objects.get(
-#                 user=request.user,
-#                 event_id=event_id,
-#                 left_at__isnull=True
-#             )
-#         except EventAttendance.DoesNotExist:
-#             return Response({"error": "Not attending this event"}, status=400)
+    def post(self, request, event_id):
+        try:
+            attendance = EventAttendance.objects.get(
+                user=request.user,
+                event_id=event_id,
+                left_at__isnull=True
+            )
+        except EventAttendance.DoesNotExist:
+            return Response({"error": "Not attending this event"}, status=400)
 
-#         attendance.left_at = timezone.now()
-#         attendance.save()
+        attendance.left_at = timezone.now()
+        attendance.save()
 
-#         return Response({"message": "Left event successfully"})
+        return Response({"message": "Left event successfully"})
