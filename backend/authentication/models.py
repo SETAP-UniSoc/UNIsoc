a191919a15235696ee5dae70ed9340f0f9183c99
stuti <up2199677@myport.ac.uk>
2026-03-02 14:32:47 +0000

    added string method to society model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -81,6 +81,10 @@ class Society(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+    
+    
 class SocietyAdmin(models.Model):
     ROLE_CHOICES = [
         ('president', 'President'),
