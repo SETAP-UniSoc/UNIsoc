cab558bd7f55ce5a2a4c87cb7310619f59b7c70f
MM674294 <mayamartey@gmail.com>
2026-03-04 05:19:26 +0000

    made last changes

--- lib/screens/admin/admin_hompage.dart
+++ lib/screens/admin/admin_hompage.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:carousel_slider/carousel_slider.dart';
 import 'admin_bottom_nav.dart';
 import 'admin_dropdown_menu.dart';
