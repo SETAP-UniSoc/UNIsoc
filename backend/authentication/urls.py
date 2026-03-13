29b9d626ecfce9155b80231f7c2f17102729b616
MM674294 <mayamartey@gmail.com>
2026-03-13 16:11:40 +0000

    made society view file

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .loginAPIview import LoginView
-
+from .SocietyView import SocietyListView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+     path("societies/", SocietyListView.as_view(), name="societies"),
 ]
 
