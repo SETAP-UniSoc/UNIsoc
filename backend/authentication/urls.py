931bc2e76f76327c7a42864ebfabcff2fd5c3904
MM674294 <mayamartey@gmail.com>
2026-03-13 16:11:40 +0000

    made society view file

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,24 +1,7 @@
 from django.urls import path
-from .RegisterView import RegisterView
-from .LoginView import LoginView
-from .AnalyticsView import AnalyticsView
-from .JoinSoc import JoinSocietyView
-from .LeaveSoc import LeaveSocietyView
-from .EventView import EventListCreateView, EventDetailView #M added
+from .loginAPIview import LoginView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("user/register/", RegisterView.as_view(), name="register"),
-    path("society/<int:society_id>/join/", JoinSocietyView.as_view(), name="join-society"),
-    path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
-    path('my-analytics/', AnalyticsView.as_view(), name="society-analytics"),
-    path("society/<int:society_id>/events/", EventListCreateView.as_view(), name="society-events"), #M added
-    path("event/<int:event_id>/delete/", EventDetailView.as_view(), name="event-detail"), #M added
+    path('login/', LoginView.as_view(), name='login'),
 ]
-
-
-
-
-
-
 
