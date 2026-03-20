5e6e3562a2a07cac0d6404ee6bf328151f6049c9
stuti <up2199677@myport.ac.uk>
2026-03-20 20:26:21 +0000

    fixed urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .RegisterView import RegisterView
-from .LoginView import LoginView
-from .AnalyticsView import AnalyticsView
-from .JoinSoc import JoinSocietyView
-from .LeaveSoc import LeaveSocietyView
-from .EventView import EventListCreateView, EventDetailView #M added
+from .loginAPIview import LoginView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path("login/", LoginView.as_view(), name="login"),
+    path("user/register/", RegisterView.as_view(), name="register"),
+    path("society/<int:society_id>/join/", JoinSocietyView.as_view(), name="join-society"),
+    path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
+    path('my-analytics/', AnalyticsView.as_view(), name="society-analytics")
+
 ]
+
+
+
+
+
+
 
