e53fd316cf8798301b7ac131f0ecd0a64fa023d7
stuti <up2199677@myport.ac.uk>
2026-03-20 20:17:10 +0000

    added path to urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from .loginAPIview import LoginView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path("login/", LoginView.as_view(), name="login"),
+    path("user/register/", RegisterView.as_view(), name="register"),
+    path("society/<int:society_id>/join/", JoinSocietyView.as_view(), name="join-society"),
+    path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
+    path('my-analytics/', AnalyticsView.as_view(), name="society-analytics")
+
 ]
+
+
+
+
+
+
 
