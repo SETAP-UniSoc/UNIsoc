05755f14326647adacb0f4f0cf13b8d1bfc73081
stuti <up2199677@myport.ac.uk>
2026-03-20 20:17:10 +0000

    added path to urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -7,18 +7,6 @@ from .LeaveSoc import LeaveSocietyView
 from .EventView import EventListCreateView, EventDetailView #M added
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("user/register/", RegisterView.as_view(), name="register"),
-    path("society/<int:society_id>/join/", JoinSocietyView.as_view(), name="join-society"),
-    path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
-    path('my-analytics/', AnalyticsView.as_view(), name="society-analytics"),
-    path("society/<int:society_id>/events/", EventListCreateView.as_view(), name="society-events"), #M added
-    path("event/<int:event_id>/delete/", EventDetailView.as_view(), name="event-detail"), #M added
+    path('login/', LoginView.as_view(), name='login'),
 ]
-
-
-
-
-
-
 
