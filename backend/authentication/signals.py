77f7504c838864047f98bca626c2a97b89a8afed
stuti <up2199677@myport.ac.uk>
2026-03-20 19:51:46 +0000

    addes a trigger to signals.py to send email to user

---
+++ backend/authentication/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Event
+from .services import send_event_notifications
+
+@receiver(post_save, sender=Event)
+def event_created(sender, instance, created, **kwargs):
+    if created:
+        send_event_notifications(instance)
