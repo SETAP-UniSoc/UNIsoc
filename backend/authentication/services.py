ac57cda40873f283d96b1c694946d54bb2a4e9dc
stuti <up2199677@myport.ac.uk>
2026-03-20 19:50:13 +0000

    added send_event_notifications to services.py

--- backend/authentication/services.py
+++ backend/authentication/services.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db.models import Q
+from django.core.mail import send_mail
+from .models import NotificationPreference
 
 User = get_user_model()
 
@@ -15,5 +17,21 @@ def authenticate_user(identifier, password):
         return user
 
     return None
+
+def send_event_notifications(event):
+    prefs = NotificationPreference.objects.filter(
+        society=event.society,
+        event_notifications=True
+    )
+
+    emails = [p.user.email for p in prefs]
+
+    if emails:
+        send_mail(
+            subject=f"New Event: {event.title}",
+            message=f"{event.description}\nLocation: {event.location}",
+            from_email="your_email@gmail.com",
+            recipient_list=emails,
+        )
 
 
