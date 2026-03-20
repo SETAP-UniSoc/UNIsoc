05be0cb996685d9b8dae35dbf040ad530721049c
MM674294 <mayamartey@gmail.com>
2026-03-20 20:22:34 +0000

    made changes

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,7 +4,9 @@ from .LoginView import LoginView
 from .AnalyticsView import AnalyticsView
 from .JoinSoc import JoinSocietyView
 from .LeaveSoc import LeaveSocietyView
-from .SocietyDetailView import SocietyDetailView
+from .EventView import EventListCreateView, EventDetailView #M added
+from .SocietyView import SocietyDetailView, SocietyListView #M added
+from .SocietyView import SocietyListView, SocietyDetailView, SocietyMembershipCheckView #M added
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -13,6 +15,7 @@ urlpatterns = [
     path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
     path('my-analytics/', AnalyticsView.as_view(), name="society-analytics")
     path('societies/<int:society_id>/', SocietyDetailView.as_view(), name='society-detail'),
+    path('customers/', CustomerListView.as_view(), name='customer-list-create'),
 ]
 
 
