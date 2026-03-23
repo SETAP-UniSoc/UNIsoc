2f9f065b1fbd58e9c07882ef48bc50549280928a
stuti <up2199677@myport.ac.uk>
2026-03-23 20:09:25 +0000

    made chages

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-
-from UNIsoc.backend.authentication.views import CustomerListView
 from .RegisterView import RegisterView
 from .LoginView import LoginView
 from .AnalyticsView import AnalyticsView
 from .JoinSoc import JoinSocietyView
 from .LeaveSoc import LeaveSocietyView
 from .SocietyDetailView import SocietyDetailView
-from .views import CustomerListView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -16,8 +13,8 @@ urlpatterns = [
     path("society/<int:society_id>/leave/", LeaveSocietyView.as_view(), name="leave-society"),
     path('my-analytics/', AnalyticsView.as_view(), name="society-analytics"),
     path('societies/<int:society_id>/', SocietyDetailView.as_view(), name='society-detail'),
-    path('customers/', CustomerListView.as_view(), name='customer-list-create'),
 ]
+
 
 
 
