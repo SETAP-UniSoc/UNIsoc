cbce274137d0d5a82d5b0d9faa1f7df1e1ebbc35
stuti <up2199677@myport.ac.uk>
2026-03-23 20:11:15 +0000

    made urls correct

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -19,10 +19,14 @@ from .ChangePasswordView import ChangePasswordView #M added
 =======
 from .SocietyDetailView import SocietyDetailView
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .views import CustomerListView
 >>>>>>> c58e0ce2a04e1a656a1de5f17a638a5d99df0022
 =======
 >>>>>>> 2f9f065 (made chages)
+=======
+from .views import CustomerListCreateView
+>>>>>>> 0ce5e3a (made urls correct)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -38,8 +42,11 @@ urlpatterns = [
     path("change-password/", ChangePasswordView.as_view(), name="change-password"), #M added
 =======
     path('societies/<int:society_id>/', SocietyDetailView.as_view(), name='society-detail'),
+<<<<<<< HEAD
     path('customers/', CustomerListView.as_view(), name='customer-list-create'),
 >>>>>>> c58e0ce2a04e1a656a1de5f17a638a5d99df0022
+=======
+>>>>>>> 2f9f065 (made chages)
 ]
 
 
