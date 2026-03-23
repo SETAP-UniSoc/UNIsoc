13e3508a482099bf4dad622738d4aa1191ddb4a1
MM674294 <mayamartey@gmail.com>
2026-03-23 20:31:14 +0000

    changed soc profile page again

--- lib/screens/society_profile_page.dart
+++ lib/screens/society_profile_page.dart
@@ -186,7 +186,10 @@ class _SocietyProfilePageState extends State<SocietyProfilePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+<<<<<<< HEAD
+=======
 
+>>>>>>> 495c1ca7b53d19b3599765ed7ff7b13e6dd146ff
                   // society header card
                   Container(
                     width: double.infinity,
@@ -344,3 +347,4 @@ class _SocietyProfilePageState extends State<SocietyProfilePage> {
     );
   }
 }
+>>>>>>> 495c1ca7b53d19b3599765ed7ff7b13e6dd146ff
