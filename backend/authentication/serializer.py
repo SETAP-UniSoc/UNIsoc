1682cda59c9930c8204cd861ec64dd863bcb1701
stuti <up2199677@myport.ac.uk>
2026-03-24 18:41:43 +0000

    chnaged custome rot user

--- backend/authentication/serializer.py
+++ backend/authentication/serializer.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 from .models import Society, User
 
-class CustomerSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Society
         fields = '__all__'
