6f3488b462a46ca5d880c9b44e9b2211e0f5a15d
MM674294 <mayamartey@gmail.com>
2026-03-24 19:18:36 +0000

    made changes

--- lib/screens/admin/admin_hompage.dart
+++ lib/screens/admin/admin_hompage.dart
@@ -78,7 +78,7 @@ class _AdminHomepageState extends State<AdminHomepage> {
   Future<void> loadSocieties() async {
     try {
       final response = await http.get(
-        Uri.parse("${ApiService.baseUrl}/societies/"),
+        Uri.parse("${ApiService.baseUrl}/api/societies/"),
         headers: ApiService.headers,
       );
       if (response.statusCode == 200) {
@@ -96,7 +96,7 @@ class _AdminHomepageState extends State<AdminHomepage> {
   Future<void> loadEvents() async {
     try {
       final response = await http.get(
-        Uri.parse("${ApiService.baseUrl}/events/"),
+        Uri.parse("${ApiService.baseUrl}/api/events/"),
         headers: ApiService.headers,
       );
       if (response.statusCode == 200) {
@@ -106,7 +106,7 @@ class _AdminHomepageState extends State<AdminHomepage> {
     } catch (e) {
       print("Error loading events: $e");
       setState(() => events = []);
-    }
+    } print("societies legnth: ${societies.length}");
   }
 
   void applyFilters() {
