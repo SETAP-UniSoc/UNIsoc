03840db4f7a83238c7f03927b8f0c3d1d8b366da
stuti <up2199677@myport.ac.uk>
2026-03-24 19:01:27 +0000

    chnagee urls to fir userlistview

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -20,6 +20,7 @@ from .ChangePasswordView import ChangePasswordView #M added
 from .SocietyDetailView import SocietyDetailView
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .views import CustomerListCreateView
 =======
 from .views import CustomerListView, SocietyListView
@@ -27,6 +28,9 @@ from .views import CustomerListView, SocietyListView
 =======
 from .views import UserListView, SocietyListView
 >>>>>>> cb3eb00 (made chnages sos guys it will work now)
+=======
+from .views import UserListView, SocietyListView
+>>>>>>> 3aac1bc (chnagee urls to fir userlistview)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -43,9 +47,13 @@ urlpatterns = [
 =======
     path('societies/<int:society_id>/', SocietyDetailView.as_view(), name='society-detail'),
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('customers/', CustomerListCreateView.as_view(), name='customer-list-create'),
 =======
     path('customers/', CustomerListView.as_view(), name='customer-list-create'),
+=======
+    path('users/', UserListView.as_view(), name='user-list-create'),
+>>>>>>> cb3eb00 (made chnages sos guys it will work now)
     path('societies/', SocietyListView.as_view(), name='society-list-create'),
 >>>>>>> 62d4619 (made chnages so url and added path to import socs)
 ]
