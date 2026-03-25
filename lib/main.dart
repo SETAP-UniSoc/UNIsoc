539efc7a0ba0a6e00fc3b48cedb191bb96b0fc7a
MM674294 <mayamartey@gmail.com>
2026-03-25 18:07:50 +0000

    corrected main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'screens/login_screen.user.dart';
 
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginScreenUser(),
     );
   }
 }
