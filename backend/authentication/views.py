c7db6ca54fc1c632419af8074eb3f22f19c49acd
stuti <up2199677@myport.ac.uk>
2026-03-26 21:34:51 +0000

    chnaged views.py to make events global

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -175,9 +175,10 @@ class AllEventsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        events = Event.objects.filter(
-            start_time__gte=now()   # ✅ ONLY FUTURE EVENTS
-        ).order_by('start_time')[:5]  # ✅ SOONEST FIRST
+        events = Event.objects.all().order_by('-created_at')[:5]
+        # events = Event.objects.filter(
+        #     start_time__gte=now()   # ✅ ONLY FUTURE EVENTS
+        # ).order_by('start_time')[:5]  # ✅ SOONEST FIRST
 
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
