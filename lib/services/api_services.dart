2bdcfdd988de72a922477d2c0bc16c308955ac6a
MM674294 <mayamartey@gmail.com>
2026-03-30 20:09:38 +0100

    changed ip address

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "http://10.128.4.122:8000/api";
+  static const String baseUrl = "http://10.128.5.136:8000/api";
 
   static String? authToken;
   static int? societyId; // Admin's society ID
