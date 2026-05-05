283a2f485e77ab4b0027903c8a2e1decd7515751
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2026-05-05 14:38:08 +0100

    unit tests edited

--- test/user_test/login_screen.user_test.dart
+++ test/user_test/login_screen.user_test.dart
@@ -1,6 +1,72 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:unisoc/screens/user/login_screen.user.dart';
+
+class _MockLoginHttpOverrides extends HttpOverrides {
+  final int statusCode;
+  final List<int> responseBody;
+
+  _MockLoginHttpOverrides(this.statusCode, this.responseBody);
+
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return _MockHttpClient(statusCode, responseBody);
+  }
+}
+
+class _MockHttpClient implements HttpClient {
+  final int statusCode;
+  final List<int> responseBody;
+
+  _MockHttpClient(this.statusCode, this.responseBody);
+
+  @override
+  Future<HttpClientRequest> openUrl(String method, Uri url) async {
+    return _MockHttpClientRequest(statusCode, responseBody);
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _MockHttpClientRequest implements HttpClientRequest {
+  final int statusCode;
+  final List<int> responseBody;
+
+  _MockHttpClientRequest(this.statusCode, this.responseBody);
+
+  @override
+  Future<HttpClientResponse> close() async {
+    return _MockHttpClientResponse(statusCode, responseBody);
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _MockHttpClientResponse implements HttpClientResponse {
+  @override
+  final int statusCode;
+  final List<int> responseBody;
+
+  _MockHttpClientResponse(this.statusCode, this.responseBody);
+
+  @override
+  int get contentLength => responseBody.length;
+
+  @override
+  StreamSubscription<List<int>> listen(void Function(List<int> event)? onData,
+      {bool? cancelOnError, void Function()? onDone, Function? onError}) {
+    return Stream.value(responseBody).listen(onData,
+        cancelOnError: cancelOnError, onDone: onDone, onError: onError);
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
 
 void main() {
   Widget buildTestableWidget() {
@@ -8,6 +74,24 @@ void main() {
       home: LoginScreenUser(),
     );
   }
+
+testWidgets('invalid credentials shows error message', (WidgetTester tester) async {
+  final previous = HttpOverrides.current;
+  HttpOverrides.global = _MockLoginHttpOverrides(401, utf8.encode('{"error":"invalid upnumber or password"}'));
+
+  try {
+    await tester.pumpWidget(buildTestableWidget());
+
+    await tester.enterText(find.byType(TextField).first, '1234567');
+    await tester.enterText(find.byType(TextField).last, 'wrongpass');
+    await tester.tap(find.text('Login'));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('invalid'), findsOneWidget);
+  } finally {
+    HttpOverrides.global = previous;
+  }
+});
 
   testWidgets('renders the login screen controls', (WidgetTester tester) async {
     await tester.pumpWidget(buildTestableWidget());
