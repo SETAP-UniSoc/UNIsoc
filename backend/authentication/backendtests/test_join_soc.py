dc6483a2139db4290b35f700ac5f937869aa51fb
stuti <up2199677@myport.ac.uk>
2026-05-07 02:03:50 +0100

    test: add tests for joining society twice and rejoining after leaving

--- backend/authentication/backendtests/test_join_soc.py
+++ backend/authentication/backendtests/test_join_soc.py
@@ -1,8 +1,11 @@
+from time import timezone
+
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from authentication.models import Society, Membership
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -56,3 +59,58 @@ class JoinSocietyTests(APITestCase):
         )
 
         self.assertEqual(response.status_code, 404)
+    
+    def test_join_society_twice_returns_already_joined(self):
+
+        Membership.objects.create(
+            user=self.user,
+            society=self.society
+        )
+
+        response = self.client.post(
+            self.url,
+            {},
+            HTTP_AUTHORIZATION=f"Token {self.token.key}"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["message"], "Already joined")
+
+
+    def test_rejoin_society_after_leaving(self):
+
+        membership = Membership.objects.create(
+            user=self.user,
+            society=self.society
+        )
+
+        membership.left_at = timezone.now()
+        membership.save()
+
+        response = self.client.post(
+            self.url,
+            {},
+            HTTP_AUTHORIZATION=f"Token {self.token.key}"
+        )
+
+        membership.refresh_from_db()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["message"], "Rejoined successfully")
+        self.assertIsNone(membership.left_at)
+
+
+    def test_membership_created_correctly(self):
+
+        self.client.post(
+            self.url,
+            {},
+            HTTP_AUTHORIZATION=f"Token {self.token.key}"
+        )
+
+        membership = Membership.objects.get(
+            user=self.user,
+            society=self.society
+        )
+
+        self.assertEqual(membership.role, "member")
